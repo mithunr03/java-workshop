0dd55204a08c20d15adf40f2f9460ace339cb321
mithunr03 <mithunachu80@gmai.com>
2025-07-02 14:55:01 +0530

    total sum of marks

---
+++ src/com/vetias/java/basics/TotalMarks.java
@@ -0,0 +1,17 @@
+public class TotalMarks {
+    public static void main(String[]args){
+        int [] marks ={90,99,94,98,99};
+        int total=0;
+        for(int mark:marks){
+            total+=mark;
+        
+
+    
+    }
+    System.out.println(total);
+    
+    
+}
+}
+
+
