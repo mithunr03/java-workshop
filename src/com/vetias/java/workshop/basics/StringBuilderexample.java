cc535c636370f382a449f70606917210f37eef22
mithunr03 <mithunachu80@gmai.com>
2025-07-07 14:21:56 +0530

    a small change

--- src/com/vetias/java/workshop/basics/StringBuilderexample.java
+++ src/com/vetias/java/workshop/basics/StringBuilderexample.java
@@ -3,7 +3,7 @@ public class StringBuilderexample {
         StringBuilder sb= new StringBuilder();
         sb.append("java").append(" ").append("is").append(" ").append("the").append(" ").append("most").append("  ")
         .append("used").append(" ").append("language").append(" ").append("for").append(" ").append("enterprices").append(" ")
-        .append(" ").append("application");
+        .append("application");
         String restult=sb.toString();
         System.out.println(restult);        
     }
