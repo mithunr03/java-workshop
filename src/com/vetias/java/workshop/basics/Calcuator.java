f8de441a8d6149e66e2e42cf00b81cf9f4765f88
mithunr03 <mithunachu80@gmai.com>
2025-07-08 14:58:23 +0530

    new

---
+++ src/com/vetias/java/workshop/basics/Calcuator.java
@@ -0,0 +1,39 @@
+public class Calcuator {
+    public int  division(int a ,int b)
+    {
+     try{
+        if(b==0)
+        {
+            throw new ArithmeticException("Divided by zero is not possible");
+            return 0;
+        }
+    
+        return a/b;
+     }
+    
+     catch (ArithmeticException e)
+     {
+        System.out.println(e.getMessage());
+        return 0;
+
+     }
+    
+     public int addition(int a,int b)
+     {
+        return a+b;
+     }
+     public int subtraction( int a ,int b)
+     {
+        return a-b;
+     }
+     public static void main(String[] args)
+      {
+        Calcuator calcuator=new Calcuator();
+        System.out.println("addition"+calcuator.addition(5,6));
+        System.out.println("subtraction"+calcuator.subtraction(5,6));
+        System.out.println("division"+calcuator.division(5,0));
+     }
+    
+}
+
+
