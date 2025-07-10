87018b96ad3081ebc866f4b7d604f7e0f6f9c40e
mithunr03 <mithunachu80@gmai.com>
2025-07-10 15:03:19 +0530

    new

---
+++ src/main/java/com/vetias/java/workshop/collections/ListExample.java
@@ -0,0 +1,25 @@
+package com.vetias.java.workshop.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListExample {
+    public static void main(String[] args) {
+        
+    
+    List<String> names= new ArrayList<>();
+    names.add("achu");
+    names.add("mithun");
+    names.add("jack");
+    names.add("kate");
+    names.add("locke");
+    System.out.println(names.set(1, "achu"));
+    for(int i=0;i<names.size();i++)
+    {
+       System.out.println(names.get(i));
+    }
+    
+
+    }
+
+}
